a4e50975217d098b6b4559beb4fe0122fe87c3dd
Arthur Silva <fuscellaarthur@gmail.com>
2021-06-08 21:57:52 -0300

    feat(dto): add invoice dto objects

---
+++ backend/src/main/java/com/invoice/backend/models/dto/InvoiceDTO.java
@@ -0,0 +1,55 @@
+package com.invoice.backend.models.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import com.invoice.backend.entities.Invoice;
+import com.invoice.backend.entities.InvoiceItems;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class InvoiceDTO implements Serializable {
+
+	private UUID uuid;
+
+	private String fiscalNumber;
+
+	private String model;
+
+	private BigDecimal total;
+
+	private Instant issuedAt;
+
+	private List<InvoiceItemsDTO> items = new ArrayList<>();
+
+	private Instant createdAt;
+
+	private Instant updatedAt;
+
+	public InvoiceDTO(Invoice invoice) {
+		uuid = invoice.getUuid();
+		fiscalNumber = invoice.getFiscalNumber();
+		model = invoice.getModel();
+		total = invoice.getTotal();
+		issuedAt = invoice.getIssuedAt();
+		createdAt = invoice.getCreatedAt();
+		updatedAt = invoice.getUpdatedAt();
+	}
+
+	public InvoiceDTO(Invoice invoice, Set<InvoiceItems> invoiceItems) {
+		this(invoice);
+		invoiceItems.forEach(items -> this.getItems().add(new InvoiceItemsDTO(items)));
+	}
+
+}
---
+++ backend/src/main/java/com/invoice/backend/models/dto/InvoiceItemsDTO.java
@@ -0,0 +1,50 @@
+package com.invoice.backend.models.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+import com.invoice.backend.entities.InvoiceItems;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class InvoiceItemsDTO implements Serializable {
+
+	private UUID uuid;
+
+	private Integer item;
+
+	private String description;
+
+	private String unitOfMeasure;
+
+	private Integer quantity;
+
+	private BigDecimal priceUnit;
+
+	private BigDecimal total;
+
+	private Instant createdAt = Instant.now();
+
+	private Instant updatedAt = Instant.now();
+
+	public InvoiceItemsDTO(InvoiceItems invoiceItems) {
+		this.uuid = invoiceItems.getUuid();
+		this.item = invoiceItems.getItem();
+		this.description = invoiceItems.getDescription();
+		this.unitOfMeasure = invoiceItems.getUnitOfMeasure();
+		this.quantity = invoiceItems.getQuantity();
+		this.priceUnit = invoiceItems.getPriceUnit();
+		this.total = invoiceItems.getTotal();
+		this.createdAt = invoiceItems.getCreatedAt();
+		this.updatedAt = invoiceItems.getUpdatedAt();
+	}
+
+}
