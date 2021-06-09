1bcbe35a1ce20555b8c9b3c86707e4ddcec49813
Arthur Silva <fuscellaarthur@gmail.com>
2021-06-08 21:54:38 -0300

    feat(utils): add constants and i18n resource bundle

---
+++ backend/src/main/java/com/invoice/backend/utils/BackendConstants.java
@@ -0,0 +1,10 @@
+package com.invoice.backend.utils;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class BackendConstants {
+
+	public static final ResourceBundle BUNDLE = ResourceBundle.getBundle("i18n/i18n", Locale.getDefault());
+
+}
---
+++ backend/src/main/java/com/invoice/backend/utils/MessagesConstants.java
@@ -0,0 +1,7 @@
+package com.invoice.backend.utils;
+
+public class MessagesConstants {
+
+	public static final String NOT_FOUND = BackendConstants.BUNDLE.getString("exception.notFound");
+
+}
