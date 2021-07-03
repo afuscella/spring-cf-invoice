7086a4c120cb1cc3fd54585588553abcea238818
Arthur Silva <fuscellaarthur@gmail.com>
2021-07-02 21:31:38 -0300

    test(it): add integration test of context load

--- backend/src/test-integration/java/com/invoice/backend/BackendApplicationITests.java
+++ backend/src/test-integration/java/com/invoice/backend/BackendApplicationITests.java
@@ -1,13 +1,15 @@
 package com.invoice.backend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class BackendApplicationITests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertDoesNotThrow(() -> this);
 	}
 
 }
